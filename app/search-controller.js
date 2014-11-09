6c19242c9ca738c4a7009b235af1703ed04ffa84
saltandbeer <raditude28@gmail.com>
2014-11-09 14:34:07 -0500

    implment search

--- app/search-controller.js
+++ app/search-controller.js
@@ -1,7 +1,12 @@
-aardvarkweb.SearchCtrl = ['API', function(API) {
+aardvarkweb.SearchCtrl = ['API', '$scope', '$location',
+function(API, $scope, $location) {
 
     var $this = this;
     this.twitterUsers = [];
+
+    this.search = function () {
+      $location.path($scope.searchText);
+    };
 
     API.getTwitterUsers()
         .then(function(users) {
