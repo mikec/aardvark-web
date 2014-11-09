0923d8d716023b0c7643a1c2302b63d451565a7d
Mike Calvanese <mfcalvanese@gmail.com>
2014-11-09 01:01:35 -0500

    Added rdio call for artists

--- app/playlist-controller.js
+++ app/playlist-controller.js
@@ -1,15 +1,22 @@
-aardvarkweb.PlaylistCtrl = ['$routeParams', 'API',
-function($routeParams, API) {
+aardvarkweb.PlaylistCtrl = ['$routeParams', 'API', 'PlaylistMaker',
+function($routeParams, API, PlaylistMaker) {
 
     var $this = this;
     this.handle = $routeParams.handle;
     this.twitterUser = null;
     this.loading = true;
+    this.playlist = [];
 
     API.getTwitterUser(this.handle)
         .then(function(twitterUser) {
             $this.loading = false;
             $this.twitterUser = twitterUser;
+
+            PlaylistMaker.getPlaylist(['r48259', 'r2291142'])
+                .then(function(playlist) {
+                    console.log(playlist);
+                    $this.playlist = playlist;
+                });
         });
 
 }];
--- app/playlist-maker-service.js
+++ app/playlist-maker-service.js
@@ -1,6 +1,33 @@
-aardvarkweb.PlaylistMaker = [function() {
+aardvarkweb.PlaylistMaker = ['$rootScope', '$q', function($rootScope, $q) {
 
+    function PlaylistMaker() {}
 
+    PlaylistMaker.prototype.getPlaylist = function(artistIds) {
+
+        var def = $q.defer();
+
+        R.request({
+            method: 'get',
+            content: {
+                keys: artistIds.join(',')
+            },
+            success: function(resp) {
+                var songs = [];
+                for(var i in resp.result) {
+                    songs.push(resp.result[i]);
+                }
+                def.resolve(songs);
+            },
+            error: function(err) {
+                def.reject(err);
+            }
+        });
+
+        return def.promise;
+
+    };
+
+    return new PlaylistMaker();
 
 }];
 
